7c5e3350d6ac29570701b7e62d1d82ca4f1cb41b
Andrew DeVoss <apd0340@utulsa.edu>
2022-03-28 16:56:31 -0500

    mentheboys thinkingaboutthinking

--- project/Utils/test.py
+++ project/Utils/test.py
@@ -1,1 +1,14 @@
-print('hello')
+from matplotlib import image, pyplot
+import os
+
+cwd = os.getcwd()
+utils = cwd+"\\project\\Utils\\"
+# load image as pixel array
+me_n_the_boys = image.imread(utils+'thinking_about_thinking.jpg')
+# summarize shape of the pixel array
+print(me_n_the_boys.dtype)
+print(me_n_the_boys.shape)
+print(me_n_the_boys)
+# display the array of pixels as an image
+pyplot.imshow(me_n_the_boys)
+pyplot.show()
