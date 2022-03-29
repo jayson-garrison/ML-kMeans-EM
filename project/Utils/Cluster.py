e275514873cf9f7bb602fa68a2c51fe34884bb2b
Andrew DeVoss <apd0340@utulsa.edu>
2022-03-28 20:15:47 -0500

    adding some util files

---
+++ project/Utils/Cluster.py
@@ -0,0 +1,10 @@
+class Cluster:
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def removePoint(self, x):
+        self.X.remove(x)
+
+    def addPoint(self, x):
+        self.X.add(x)
---
+++ project/Utils/Sample.py
@@ -0,0 +1,13 @@
+class Sample:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+
+    def setY(self, y):
+        self.y = y
